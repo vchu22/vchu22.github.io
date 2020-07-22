db4e2013272d9bbe915cd2f400f9185922e9238f
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2020-07-22 18:33:13 -0400

    add ability to close by clicking outside the modal

--- js/angular/directives/modal.js
+++ js/angular/directives/modal.js
@@ -1,16 +1,15 @@
-app.directive('modal', function() {
+app.directive("modal", function () {
   return {
-    restrict: 'E',
+    restrict: "E",
     scope: {
-      item: '=',
+      item: "=",
     },
-    templateUrl: 'js/angular/directives/modal.html',
-    controller: function($scope) {
-      $scope.open = function(url) {
-        console.log(typeof url);
-        window.open(url, '_blank');
+    templateUrl: "js/angular/directives/modal.html",
+    controller: function ($scope) {
+      $scope.open = function (url) {
+        window.open(url, "_blank");
       };
-      $scope.close = function() {
+      $scope.close = function () {
         $scope.$parent.show = false;
       };
     },
